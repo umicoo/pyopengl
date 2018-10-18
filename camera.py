cb552e1ad924bae834b8172a366fd1ac850231e3
umicoo <35709022+umicoo@users.noreply.github.com>
2018-10-18 13:35:20 +0100

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,38 +1,57 @@
-class Camera:
-	def __init__():
-		PI = 3.1415
-		angle=3
-		speed=0.3
-		sight=100
+import math
 
-		rotate_yz=0.0
-		rotate_xz=-90.0
-		rad_yz=rotate_yz*PI/180.0
-		rad_xz=rotate_xz*PI/180.0
+class Camera():
+	def __init__(self):
+		self.PI = 3.1415
+		self.angle=3
+		self.speed=0.3
+		self.sight=100
 
-		camera_x=0.0
-		camera_y=0.0
-		camera_z=0.0
+		self.rotate_yz=0.0
+		self.rotate_xz=-90.0
+		self.rad_yz=self.rotate_yz*self.PI/180.0
+		self.rad_xz=self.rotate_xz*self.PI/180.0
 
-		lookat_x= camera_x + sight*cos(rad_yz)*cos(rad_xz)
-		lookat_y= camera_y + sight*sin(rad_yz)
-		lookat_z= camera_z + sight*cos(rad_yz)*sin(rad_xa)
+		self.camera_x=0.0
+		self.camera_y=0.0
+		self.camera_z=0.0
 
-	def YawCamera(fAngle):
-		rotate_xz=int(rotate_xz+fAngle)%360
-		rad_xz=rotate_xz*PI / 180.0
+		self.lookat_x= self.camera_x + self.sight*math.cos(self.rad_yz)*math.cos(self.rad_xz)
+		self.lookat_y= self.camera_y + self.sight*math.sin(self.rad_yz)
+		self.lookat_z= self.camera_z + self.sight*math.cos(self.rad_yz)*math.sin(self.rad_xz)
 
-		lookat_x = camera_x + sight*cos(rad_yz)*cos(rad_xz)
-		lookat_y = camera_y + sight*sin(rad_yz)
-		lookat_z = camera_z + sight*cos(rad_yz)*sin(rad_xz)
+	def YawCamera(self,fAngle):
+		self.rotate_xz=int(self.rotate_xz+fAngle)%360
+		self.rad_xz=self.rotate_xz*self.PI / 180.0
 
-	def PitchCamera(fAngle):
-		rotate_xz=int(rotate_yz+fAngle)%360
-		rad_yz=rotate_yz*PI/180.0
+		self.lookat_x = self.camera_x + self.sight*math.cos(self.rad_yz)*math.cos(self.rad_xz)
+		self.lookat_y = self.camera_y + self.sight*math.sin(self.rad_yz)
+		self.lookat_z = self.camera_z + self.sight*math.cos(self.rad_yz)*math.sin(self.rad_xz)
 
-		lookat_x = camera_x + sight*cos(rad_yz)*cos(rad_xz)
-		lookat_y = camera_y + sight*sin(rad_yz)
-		lookat_z = camera_z + sight*cos(rad_yz)*sin(rad_xz)
+	def PitchCamera(self,fAngle):
+		self.rotate_yz=int(self.rotate_yz+fAngle)%360
+		self.rad_yz=self.rotate_yz*self.PI/180.0
+
+		self.lookat_x = self.camera_x + self.sight*math.cos(self.rad_yz)*math.cos(self.rad_xz)
+		self.lookat_y = self.camera_y + self.sight*math.sin(self.rad_yz)
+		self.lookat_z = self.camera_z + self.sight*math.cos(self.rad_yz)*math.sin(self.rad_xz)
+
+	def WalkStraight(self, fSpeed):
+		self.camera_x =self.camera_x+fSpeed*math.cos(self.rad_yz)*math.cos(self.rad_xz)
+		self.camera_y =self.camera_y+fSpeed*math.sin(self.rad_yz)
+		self.camera_z =self.camera_z+fSpeed*math.cos(self.rad_yz)*math.sin(self.rad_xz)
+
+		self.lookat_x = self.camera_x + self.sight*math.cos(self.rad_yz)*math.cos(self.rad_xz)
+		self.lookat_y = self.camera_y + self.sight*math.sin(self.rad_yz)
+		self.lookat_z = self.camera_z + self.sight*math.cos(self.rad_yz)*math.sin(self.rad_xz)
+
+	def WalkTransverse(self, fSpeed):
+		self.camera_x+=fSpeed*math.cos(self.rad_yz)*math.sin(self.rad_xz)
+		self.camera_z-=fSpeed*math.cos(self.rad_yz)*math.cos(self.rad_xz)
+
+		self.lookat_x = self.camera_x + self.sight*math.cos(self.rad_yz)*math.cos(self.rad_xz)
+		self.lookat_y = self.camera_y + self.sight*math.sin(self.rad_yz)
+		self.lookat_z = self.camera_z + self.sight*math.cos(self.rad_yz)*math.sin(self.rad_xz)
 
 
 
